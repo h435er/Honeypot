12743af1cca1c1c6ceac82f6b1dc601c71119478
h435er <126166023+h435er@users.noreply.github.com>
2024-05-20 05:22:18 +0000

    Update honey.sh

--- honey.sh
+++ honey.sh
@@ -1,12 +1,13 @@
+  GNU nano 8.0       honey.sh
 #Author: h435er
 #A simple honeypot program
 arp_output=$(exec arp -a)
-expected_ip=("ip1" "ip2" "ip3")
-for ip in ${expected_ip[@]}; do
+expected_ip=("192.168.30.1" "ip3" "ip3")
+for ip in $expected_ip; do
     if grep -q "$ip" <<< "$arp_output"; then
       echo "Alles Gut" >> log.txt
     else
       curl -d "EINDRINGLING!!!!!" ntfy.sh/honey
-      echo "EINDRINGLING!!!!" > log.txt
+      echo "EINDRINGLING!!!!" >> log.txt
     fi
 done
