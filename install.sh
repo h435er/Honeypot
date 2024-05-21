312797697f1aca25d2ceaceffe5318a49ed9c23b
h435er <126166023+h435er@users.noreply.github.com>
2024-05-21 19:33:21 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,45 +1,26 @@
 #Author: h435er
 echo """
 
-
-
-
-
-  ___ ___                              .___                 __         .__  .__   
- /   |   \  ____   ____   ____ ___.__. |   | ____   _______/  |______  |  | |  |  
-/    ~    \/  _ \ /    \_/ __ <   |  | |   |/    \ /  ___/\   __\__  \ |  | |  |  
-\    Y    (  <_> )   |  \  ___/\___  | |   |   |  \\___ \  |  |  / __ \|  |_|  |__
- \___|_  / \____/|___|  /\___  > ____| |___|___|  /____  > |__| (____  /____/____/
-       \/             \/     \/\/               \/     \/            \/           
-
-
-
+ INSTALLING HONEY
+ 
 """
 
 echo "moving the service file to /etc/systemd/system/ ..."
-#sudo mv honey.service /etc/systemd/system/
+sudo mv honey.service /etc/systemd/system/
 echo "moving the py script in the home directory ..."
-#sudo mv honey.py /home/$USER/
+sudo mv honey.py /home/$USER/
 echo "moving the shell script in the home directory ..."
-#sudo mv honey.sh /home/$USER/
-#cd
+sudo mv honey.sh /home/$USER/
+cd
 echo "mark the shell script as executable ..."
-#chmod +x honey.sh
+chmod +x honey.sh
 echo "starting honey service ..."
-#sudo systemctl enable --now honey
+sudo systemctl enable --now honey
 echo """
 
 
 
-
-.___                 __         .__  .__    _________                       .__          __             .___
-|   | ____   _______/  |______  |  | |  |   \_   ___ \  ____   _____ ______ |  |   _____/  |_  ____   __| _/
-|   |/    \ /  ___/\   __\__  \ |  | |  |   /    \  \/ /  _ \ /     \\____ \|  | _/ __ \   __\/ __ \ / __ | 
-|   |   |  \\___ \  |  |  / __ \|  |_|  |__ \     \___(  <_> )  Y Y  \  |_> >  |_\  ___/|  | \  ___// /_/ | 
-|___|___|  /____  > |__| (____  /____/____/  \______  /\____/|__|_|  /   __/|____/\___  >__|  \___  >____ | 
-         \/     \/            \/                    \/             \/|__|             \/          \/     \/ 
-
-
+INSTALLATION COMPLETED
                                                            
 
 """
