178e8c76c3958e581a45367137273bb5d5bb2d54
h435er <126166023+h435er@users.noreply.github.com>
2024-05-21 19:19:51 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,3 +11,28 @@ echo """
                           |___/                                
 
 """
+
+echo "moving the service file to /etc/systemd/system/ ..."
+sudo mv honey.service /etc/systemd/system/
+echo "moving the py script in the home directory ..."
+sudo mv honey.py /home/$USER/
+echo "moving the shell script in the home directory ..."
+sudo mv honey.sh /home/$USER/
+cd
+echo "mark the shell script as executable ..."
+chmod +x honey.sh
+echo "starting honey service ..."
+sudo systemctl enable --now honey
+echo """
+
+  _____           _        _ _   ______ _       _     _              _ 
+ |_   _|         | |      | | | |  ____(_)     (_)   | |            | |
+   | |  _ __  ___| |_ __ _| | | | |__   _ _ __  _ ___| |__   ___  __| |
+   | | | '_ \/ __| __/ _` | | | |  __| | | '_ \| / __| '_ \ / _ \/ _` |
+  _| |_| | | \__ \ || (_| | | | | |    | | | | | \__ \ | | |  __/ (_| |
+ |_____|_| |_|___/\__\__,_|_|_| |_|    |_|_| |_|_|___/_| |_|\___|\__,_|
+                                                                       
+                                                                       
+
+"""
+
