ef0f90bbbf3cf4010db309a73964339a52719b48
h435er <126166023+h435er@users.noreply.github.com>
2024-05-23 06:59:29 +0000

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,7 +1,7 @@
 #Author: h435er
-echo " uninstalling honey ..."
+echo " UNINSTALLING HONEY ..."
 sudo systemctl disable --now honey
 sudo rm /etc/systemd/system/honey.service
 sudo rm /home/$USER/honey.py
 sudo rm /home/$USER/honey.sh
-echo "uninstall complete!"
+echo "UNINSTALL COMPLETED!"
